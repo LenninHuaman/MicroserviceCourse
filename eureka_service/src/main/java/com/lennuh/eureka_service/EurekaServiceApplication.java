dcec721e2d9ef41128984b186f4ced68451d256a
LenninHuaman <u20201b975@upc.edu.pe>
2023-10-16 19:43:04 -0500

    fixed

--- eureka_service/src/main/java/com/lennuh/eureka_service/EurekaServiceApplication.java
+++ eureka_service/src/main/java/com/lennuh/eureka_service/EurekaServiceApplication.java
@@ -1,7 +1,8 @@
-package com.lennuh.eurekaservice;
+package com.lennuh.eureka_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
